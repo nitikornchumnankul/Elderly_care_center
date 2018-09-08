6375a93e78fba93059e565f91f85e20a9f562c48
Nitikorn Chumankul <nitikornchumnankul@gmail.com>
2018-09-08 09:37:31 +0700

    update_form_java

--- Elderly_care_center/src/app/app.module.ts
+++ Elderly_care_center/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolb
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
 
 const appRoutes: Routes = [
   
@@ -14,7 +15,8 @@ const appRoutes: Routes = [
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
